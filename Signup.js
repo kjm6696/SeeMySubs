c1fb74dc9cd716cfe26e00842284ccb5b351a623
kjm6696 <122549649+kjm6696@users.noreply.github.com>
2024-10-16 22:50:57 -0400

    Update Signup.js

--- Signup.js
+++ Signup.js
@@ -1,1 +1,24 @@
+import wixData from 'wix-data';
+import wixLocation from 'wix-location';
 
+
+$w('#signupButton').onClick(() => {
+	const name = $w('#nameInput').value;
+    const email = $w('#emailInput').value;
+    const password = $w('#passwordInput').value;
+    if (email && password && name) {
+        const newUser = {
+			      "name": name,
+            "email": email,
+            "password": password  
+        };
+        wixData.insert("Users", newUser)
+        .then((result) => {
+            console.log("User created:", result);
+    			  wixLocation.to(`/login`);
+        })
+        .catch((err) => {
+            console.log("Error during registration:", err);
+        });
+    }
+});
