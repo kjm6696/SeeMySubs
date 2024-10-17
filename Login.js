e46d9493c8dc4e2300f2a84a17deaa5860f1a5e9
kjm6696 <122549649+kjm6696@users.noreply.github.com>
2024-10-16 23:03:07 -0400

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,23 +1,14 @@
-
-
 import wixData from 'wix-data';
 import wixLocation from 'wix-location';
 
 $w('#loginButton').onClick(() => {
-	const name = $w('#nameInput2').value;
-    const email = $w('#emailInput2').value;
-    const password = $w('#passwordInput2').value;
+	const name = $w('#nameField').value;
+    const email = $w('#emailField').value;
+    const password = $w('#passwordField').value;
     if (email && password && name) {
-        wixData.query("Users")
-        .eq("email", email)
-        .eq("password", password)
-		    .eq("name", name)
-        .find()
-        .then((results) => {
+        wixData.query("Users").eq("email", email).eq("password", password).eq("name", name).find().then((results) => {
             if (results.items.length > 0) {
                 wixLocation.to(`/users/${results.items[0].name}`);
-            } else {
-                console.log("Error");
             }
         })
         .catch((err) => {
