1c0c17d3ff10c6434f24cd541cbc0e4a36481ec9
kjm6696 <122549649+kjm6696@users.noreply.github.com>
2024-10-20 20:32:40 -0400

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,17 +1,34 @@
+// Wix data library to manage data set operations.
 import wixData from 'wix-data';
+// Wix location library to travel throughout pages.
 import wixLocation from 'wix-location';
 
+//Action listener for the login button
 $w('#loginButton').onClick(() => {
-	const name = $w('#nameField').value;
+    // variables of fields
+	// const name = $w('#nameField').value;
     const email = $w('#emailField').value;
     const password = $w('#passwordField').value;
-    if (email && password && name) {
-        wixData.query("Users").eq("email", email).eq("password", password).eq("name", name).find().then((results) => {
+
+    // if no email, no password, or no name DONT PASS
+    if (email && password ) {
+
+        //Wix data query to search for desired account
+        wixData.query("Users")
+        .eq("email", email)
+        .eq("password", password)
+        // .eq("name", name)
+        //Will return linked account as results
+        .find().then((results) => {
+            // if there is an account linked
             if (results.items.length > 0) {
+                // navigate to user dashboard of linked account
                 wixLocation.to(`/users/${results.items[0].name}`);
             }
         })
+        //if any errors occur
         .catch((err) => {
+            // prints error to console
             console.log(err);
         });
     }
