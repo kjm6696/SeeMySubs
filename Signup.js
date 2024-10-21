13e18b1695c1c7044ef85ecc825f994bc10ca4b7
kjm6696 <122549649+kjm6696@users.noreply.github.com>
2024-10-20 20:32:55 -0400

    Update Signup.js

--- Signup.js
+++ Signup.js
@@ -1,22 +1,34 @@
+// Wix data library to manage data set operations.
 import wixData from 'wix-data';
+// Wix location library to travel throughout pages.
 import wixLocation from 'wix-location';
 
-
+// Action listener for submit button
 $w('#signupButton').onClick(() => {
+    // values from fields as variables
 	const name = $w('#nameField').value;
-    	const email = $w('#emailField').value;
-   	 const password = $w('#passwordField').value;
-    	if (email && password && name) {
-      	  const newUser = {
-		"name": name,
-        	"email": email,
-            	"password": password  
-       	 };
-      	  wixData.insert("Users", newUser).then((result) => {
-		wixLocation.to(`/login`);
+    const lastName = $w('#lastNameField').value;
+    const email = $w('#emailField').value;
+    const password = $w('#passwordField').value;
+
+    //check for empty fields
+    if (email && password && name && lastName) {
+
+        //create table to create new user
+        const newUser = {
+			"name": name,
+            "email": email,
+            "password": password,
+            "lastName": lastName
+        };
+        // wix data insert to insert new user information into database "User"
+        wixData.insert("Users", newUser).then((result) => {
+            // Navigate to login page upon new user creation success 
+			wixLocation.to(`/login`);
         })
+        //print errors if any occur durring signup process
         .catch((err) => {
-            	console.log(err);
+            console.log(err);
         });
     }
 });
